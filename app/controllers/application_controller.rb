7cee675976a739c29fae619f8bfee46824014248
Aayush Naik <aayushnaik17@gmail.com>
2015-08-28 19:59:24 +0000

    Added something.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def my_map()
+    a = [1, 2, 3, 4, 5]
+    return a.map {|i| i+1}
+  end
 end
